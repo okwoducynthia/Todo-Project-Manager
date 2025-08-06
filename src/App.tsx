f2446ea8455198d1c528c1a7c9e8774595d7ac58
okwoducynthia <okwoducynthia@gmail.com>
2025-08-06 13:59:53 +0100

    commit deleted pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,6 @@ import './App.css'
 import RegisterPage from './Pages/RegisterTaskPage/RegisterPage'
 import FetchTaskPage from './Pages/FetchAllTaskPage/FetchTaskPage'
 import UpdateTaskPage from './Pages/UpdateTaskPage/UpdateTaskPage'
-import Home from './Components/Home/Home'
-import SignUpPage from './Pages/SignUpPage/SignUpPage'
-import LoginPage from './Pages/LoginPage/LoginPage'
 
 
 
@@ -14,9 +11,6 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/sign-up' element={<SignUpPage/>}/>
-        <Route path='/login' element={<LoginPage/>}/>
         <Route path='/taskregister' element={<RegisterPage/>}/>
         <Route path='/allprojects' element={<FetchTaskPage/>}/>
         <Route path='/update-task/:id' element={<UpdateTaskPage/>}/>
